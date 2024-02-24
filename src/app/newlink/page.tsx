ff06154e926dac360117b52fd16d04ddb7924b0f
Lokesh Umredkar <lokeshumredkar2003@gmail.com>
2024-02-24 23:04:41 +0530

    newlink page card is updated

--- src/app/newlink/page.tsx
+++ src/app/newlink/page.tsx
@@ -83,11 +83,11 @@ function NewLink() {
                                 <input type="url" name="link" placeholder='Enter link' className='input-field bg-gray-800' value={formData.link} onChange={handleChange} required />
                             </div>
                         </div>
-                        <div className='flex flex-col gap-2'>
+                        <div className='flex flex-col gap-2 '>
                             <label htmlFor="name">
                                 Description:
                             </label>
-                            <textarea name="description" id="" cols={20} rows={3} className='resize-none text-white rounded-lg p-1 bg-gray-800' placeholder='Enter Description' value={formData.description} onChange={handleChange} required></textarea>
+                            <textarea name="description" id="" cols={20} rows={3} className='resize-none text-white rounded-lg p-1 bg-gray-800 overflow-hidden' placeholder='Enter Description' value={formData.description} onChange={handleChange} required></textarea>
                         </div>
                         <div className='flex justify-center items-center'>
                             <button type="submit" className='bg-mainColor text-black p-2 rounded-lg'>
@@ -102,14 +102,14 @@ function NewLink() {
                     <h1 className='text-center text-3xl text-textColor mt-10'>My Items</h1>
                     <div className='w-full h-auto flex items-center flex-wrap overflow-y-auto mx-auto p-6 lg:gap-5 gap-2 lg:justify-normal justify-center'>
                         {cards.map((card) => (
-                            <div key={card.id} className='bg-primarybg w-[300px] h-[200px] border-2 border-mainBorder rounded-lg text-textColor flex justify-around items-center flex-col '>
+                            <div key={card.id} className='bg-primarybg w-[300px] h-[200px] border-2 border-mainBorder rounded-lg text-textColor flex justify-around items-center flex-col p-3'>
                                 <div className='relative flex items-center w-full justify-center'>
                                 <h3 className='text-center'>{card.name.toLocaleUpperCase()}</h3>
-                                <span className='text-2xl text-red-500 absolute right-1 top-[-16px] cursor-pointer'>
+                                <span className='text-2xl text-red-500  cursor-pointer absolute right-0 top-[-16px]'>
                                     <IoIosRemoveCircle onClick={() => handleRemoveCard(card.id)}/>
                                     </span>
                                 </div>
-                                <p>{card.description}</p>
+                                <p>{card.description.split(" ").slice(0,10).join(" ")+(card.description.split(" ").length > 10?"...":"")}</p>
                                 <Link className='text-mainColor border-2 border-white/70 py-1 px-4 rounded-lg flex items-center gap-1' 
                                 href={card.link} target='blank'>
                                     <span><FaExternalLinkAlt/></span>
